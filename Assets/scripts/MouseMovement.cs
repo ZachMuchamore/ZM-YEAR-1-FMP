6b193e6604347bad977d939ed52fbeb666c7d074
ZachMuchamore <146819131+ZachMuchamore@users.noreply.github.com>
2024-04-10 17:22:07 +0100

    Initial commit

---
+++ Assets/scripts/MouseMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+    void Start()
+    {
+        // locking the cursor to the middle of the screen 
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Getting mouse inputs 
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotation around the X axis (look up and down)
+        xRotation -= mouseY;
+
+        // clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        // Rotation around the Y axis (Look left and right)
+        yRotation += mouseX;
+
+        // Apply Rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
