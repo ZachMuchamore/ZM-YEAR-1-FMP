1cffebdc9b00700c936fefd23487dfde20ee883d
ZachMuchamore <146819131+ZachMuchamore@users.noreply.github.com>
2024-06-11 15:31:46 +0100

    finishing touches

--- Assets/scripts/MainMenus.cs
+++ Assets/scripts/MainMenus.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
@@ -22,11 +21,7 @@ public class MainMenus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int highScore = SaveLoadManager.instance.LoadHighScore();
-        highScoreUI.text = $"Highest Wave Achieved: {highScore}";
-
-        int ZombieScore = SaveLoadManager.instance.LoadZombieScore();
-        highScoreUI.text = $"Most Zombies Killed: {ZombieScore}";
+        
 
         mainMenu.SetActive(true);
         controlsMenu.SetActive(false);
--- Assets/scripts/TimerScript.cs
+++ Assets/scripts/TimerScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.ShaderGraph.Internal;
 
 public class TimerScript : MonoBehaviour
 {
